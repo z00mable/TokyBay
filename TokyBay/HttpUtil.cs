4d2a3ce0849e922e462e1d44a4ce270b821e7a9e
Dennis Stratmann <dennis.stratmann@zurich.com>
2026-01-01 15:05:38 +0000

    v0.4.1: add parallelism for downloading and converting

--- TokyBay/HttpUtil.cs
+++ TokyBay/HttpUtil.cs
@@ -6,23 +6,30 @@
 
         public static async Task<HttpResponseMessage> GetAsync(string url)
         {
-            httpClient.DefaultRequestHeaders.Clear();
-            return await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+            {
+                using var request = new HttpRequestMessage(HttpMethod.Get, url);
+                return await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            }
         }
 
         public static async Task<HttpResponseMessage> GetTracksAsync(string url, string audioBookId, string streamToken, string trackSrc)
         {
-            httpClient.DefaultRequestHeaders.Clear();
-            httpClient.DefaultRequestHeaders.Add("x-audiobook-id", audioBookId);
-            httpClient.DefaultRequestHeaders.Add("x-stream-token", streamToken);
-            httpClient.DefaultRequestHeaders.Add("x-track-src", trackSrc);
-            return await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+            {
+                using var request = new HttpRequestMessage(HttpMethod.Get, url);
+                request.Headers.Add("x-audiobook-id", audioBookId);
+                request.Headers.Add("x-stream-token", streamToken);
+                request.Headers.Add("x-track-src", trackSrc);
+                return await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            }
         }
 
         public static async Task<HttpResponseMessage> PostAsync(string url, HttpContent content)
         {
-            httpClient.DefaultRequestHeaders.Clear();
-            return await httpClient.PostAsync(url, content);
+            {
+                using var request = new HttpRequestMessage(HttpMethod.Post, url);
+                request.Content = content;
+                return await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            }
         }
     }
 }
